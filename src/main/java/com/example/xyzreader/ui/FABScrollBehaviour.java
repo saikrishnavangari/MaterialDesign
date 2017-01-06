402da3156ba610f56af35ffcf1355adcf7bf4c6f
saikrishnavangari <saikrishna.vangari@appscore.com.au>
2017-01-06 17:27:33 +1100

    implemented hiding FAB on scrolling in detailfragment

---
+++ src/main/java/com/example/xyzreader/ui/FABScrollBehaviour.java
@@ -0,0 +1,41 @@
+package com.example.xyzreader.ui;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * Created by krrish on 6/01/2017.
+ */
+
+public class FABScrollBehaviour extends FloatingActionButton.Behavior {
+
+    public FABScrollBehaviour(Context context, AttributeSet attributeSet) {
+        super();
+    }
+
+    @Override
+    public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child,
+                               View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+        if(dyConsumed > 0 && child.getVisibility() == View.VISIBLE){
+            child.hide();
+            Log.d("scrolling", "true");
+        } else if(dyConsumed < 0 && child.getVisibility()  != View.VISIBLE){
+            Log.d("scrolling", "true");
+            child.show();
+        }
+
+    }
+
+    @Override
+    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child,
+                                       View directTargetChild, View target, int nestedScrollAxes) {
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL
+                || super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, nestedScrollAxes);
+    }
+}
